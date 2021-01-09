cbc7d118a4553340a50c6a983c8d210c5092dd63
Sankung Sillah <ssillah306@gmail.com>
2021-01-08 21:13:01 -0500

    aadded state to my app.js file

--- updated-portfolio/src/App.js
+++ updated-portfolio/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "Sankung Sillah",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "contact", path: "/contact" },
+      ],
+      home: {
+        title: "Sankung Sillah",
+        subtitle: "My portfolio",
+        text: "Checkout my projects",
+      },
+      about: {
+        title: "About Me",
+      },
+      contact: {
+        title: "Contact Me",
+      },
+    };
+  }
+  render(){
+
+    return(
+      <div>Hello from React</div>
+    )
+  }
 }
 
 export default App;
